cf6b3d0d0cc3910756fa61cff6e7d7f5b42c72ea
jainamjainCG <jainam.jain.cg@gmail.com>
2026-08-21 16:02:54 +0530

    01 introduction assigment answers done

---
+++ 01 introduction/about_python_30.py
@@ -0,0 +1,2 @@
+print("Learning Python is fun and opens up endless possibilities for building cool projects!")
+# python is a programming language which is beginner friendly and useful in many way
---
+++ 01 introduction/welcome_15.py
@@ -0,0 +1,2 @@
+print("welcome back sir")
+# the program is write to complete assigment and submit to you sir
