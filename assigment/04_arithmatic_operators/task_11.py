c45d06539838e3f7af96b66142d6eaedee7718b6
jainamjainCG <jainam.jain.cg@gmail.com>
2026-09-04 18:14:45 +0530

    task11 done

---
+++ assigment/04_arithmatic_operators/task_11.py
@@ -0,0 +1,19 @@
+a = 10 + 5 * 2
+# expected result = 20
+print(a)
+# multiplication then addition
+b = 20 - 4 / 2
+# expected result = 18
+# division then substraction 
+print(b)
+c = 10 + 20 / 5 * 2
+# expected result = 18
+# division then multiplication then addition
+print(c)
+d = 2 + 3 * 4 ** 2
+# expected result = 50
+# exponential then multiplication then addition
+print(d)
+e = 100 - 20 // 5
+# expected result = 96
+# floor division then subtraction
