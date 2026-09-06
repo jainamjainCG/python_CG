3e9c980ce89ad98cb1aa5d4b43933b66bb0ef0de
jainamjainCG <jainam.jain.cg@gmail.com>
2026-09-06 10:56:40 +0530

    task17 , done

---
+++ assigment/04_arithmatic_operators/task_17.py
@@ -0,0 +1,6 @@
+print("jainam" + "jain")
+# print("jainam" - "jain")
+print("jaianm" *3)
+# print("jainam" /  "jain")
+
+#the line 4 and the line 2 which i commented is not working and giving TypeError
