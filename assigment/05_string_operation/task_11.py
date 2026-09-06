b0fcdb67cde8fbfe8720b8a22bd87fee53a7be63
jainamjainCG <jainam.jain.cg@gmail.com>
2026-09-06 22:05:43 +0530

    task 11 done

---
+++ assigment/05_string_operation/task_11.py
@@ -0,0 +1,6 @@
+short_word = "helloooo"
+sentence = "hellloo,howareyou"
+sentence_containing_space = "hellloo , how are you"
+print(len(short_word))
+print(len(sentence))
+print(len(sentence_containing_space))
