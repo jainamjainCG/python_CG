d26e6e8f5879208edf3f952da4c3cd696c195559
jainamjainCG <jainam.jain.cg@gmail.com>
2026-09-06 16:46:32 +0530

    task 1 done

---
+++ assigment/05_string_operation/task_1.py
@@ -0,0 +1,8 @@
+name = "jaianam"
+city = "kalol"
+favorite_programming_language = 'python'
+short_message = 'python is begginer friendly'
+print(name)
+print(city)
+print(favorite_programming_language)
+print(short_message)
