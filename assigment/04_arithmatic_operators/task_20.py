247660d59d9838a06c7703a0c437b16ac52d850f
jainamjainCG <jainam.jain.cg@gmail.com>
2026-09-06 22:12:53 +0530

    task 20 done

---
+++ assigment/04_arithmatic_operators/task_20.py
@@ -0,0 +1,9 @@
+a = 20
+b = 13
+print("addition:", a , b)
+print("subtraction:", a - b)
+print("multiplication:", a * b)
+print("division:", a / b)
+print("floor division:", a // b)
+print("exponentiation:", a ** b)
+print("modulus:", a % b)
