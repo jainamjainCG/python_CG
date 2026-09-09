c9efe48bdadf12094fed2169aadc8cfe24681742
jainamjainCG <jainam.jain.cg@gmail.com>
2026-09-09 09:10:25 +0530

    q26 modified

--- problem solving/if-else problem solving --30 problems/q26.py
+++ problem solving/if-else problem solving --30 problems/q26.py
@@ -2,9 +2,9 @@ day = int(input("enter the day"))
 month = int(input("enter the month"))
 year = int(input("enter the year"))
 
-if day>0 and day<32 and month>0 and month<13 and month==1==3==5==7==8==10==12 and year>0:
+if day>0 and day<32 and month>0 and (month<13 and month==1 or month==3 or month==5 or month==7 or month==8 or month==10 or month==12) and year>0:
     print(f"{day}/{month}/{year}")
-elif day>0 and day<31 and month>00 and month<13 and month==4==6==9==11 and year>0:
+elif day>0 and day<31 and (month>00 and month<13 and month==4 or month==6 or month==9 or month==11) and year>0:
     print(f"{day}/{month}/{year}")
 elif day>0 and day<29 and month==2 and year>0:
         print(f"{day}/{month}/{year}")
