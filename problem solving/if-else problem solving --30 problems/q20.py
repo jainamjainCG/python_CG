f3d423084d350dd2fe6da340eedf815bd28ee060
jainamjainCG <jainam.jain.cg@gmail.com>
2026-09-09 09:45:03 +0530

    q20 modified

--- problem solving/if-else problem solving --30 problems/q20.py
+++ problem solving/if-else problem solving --30 problems/q20.py
@@ -2,7 +2,7 @@ side1 = int(input("enter the side1 of triangle:"))
 side2 = int(input("enter the side2 of triangle:"))
 side3 = int(input("enter the side3 of triangle:"))
 
-if side1+side2>side3 or side2+side3>side1 or side1+side3>side2:
+if side1+side2>=side3 or side2+side3>=side1 or side1+side3>=side2:
     print("it is a triangle")
 else:
     print("it's not a triangle")
