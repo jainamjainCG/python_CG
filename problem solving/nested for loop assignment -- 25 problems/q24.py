d01748d6833db4f236ad90262a6ce5be0de44ec3
jainamjainCG <jainam.jain.cg@gmail.com>
2026-09-26 12:33:05 +0530

    code updated and new and short one leaned

--- problem solving/nested for loop assignment -- 25 problems/q24.py
+++ problem solving/nested for loop assignment -- 25 problems/q24.py
@@ -1,14 +1,20 @@
 num = int(input("enter a number :"))
-for i in range(num,0,-1):
-    for j in range(0,i):
-        if j==1:
-            print("5",end="")
-        elif j==2:
-            print("4",end="")
-        elif j==3:
-            print("3",end="")
-        elif j==4:
-            print("2",end="")
-        elif j==5:
-            print("1",end="")
+# for i in range(num,0,-1):
+#     for j in range(0,i):
+#         if j==1:
+#             print("5",end="")
+#         elif j==2:
+#             print("4",end="")
+#         elif j==3:
+#             print("3",end="")
+#         elif j==4:
+#             print("2",end="")
+#         elif j==5:
+#             print("1",end="")
+#     print()
+
+
+for i in range(num):
+    for j in range(num,i,-1):
+        print(j,end=" ")
     print()
